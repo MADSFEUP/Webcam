b8c192263947175920b6e96cdc8bd2731c661222
Francisco Maciel <francisco.ma.maciel@gmail.com>
2015-10-09 16:57:05 +0100

    Add dummy channels API

--- webcam.py
+++ webcam.py
@@ -8,5 +8,9 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/channels')
+def channels():
+    return '[{"id":"cantina","url":"http://xpto.com:20000"}]'
+
 if __name__ == '__main__':
     app.run()
--- webcam_test.py
+++ webcam_test.py
@@ -1,5 +1,7 @@
 import webcam
 import unittest
+import json
+
 
 class WebcamTestCase(unittest.TestCase):
 
@@ -11,5 +13,27 @@ class WebcamTestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert 'Hello World!' in rv.data
 
+    def test_api_channels(self):
+        rv = self.app.get('/channels')
+        assert is_json(rv.data) ##test if json
+        data = json.loads(rv.data)
+        assert is_array(data) ##test if array
+        for x in range(0, len(data)):
+            print data[x]
+           ## assert hasattr(data[x], 'id')
+           ## assert hasattr(data[x], 'url')
+
 if __name__ == '__main__':
     unittest.main()
+
+
+def is_json(myjson):
+  try:
+    json_object = json.loads(myjson)
+  except ValueError, e:
+    return False
+  return True
+
+
+def is_array(var):
+    return isinstance(var, (list, tuple))
